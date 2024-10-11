063aa2e5c61457b6e95041c94a62130f1ded0dc2
Thomas Haller <thaller@redhat.com>
2024-10-11 09:44:59 +0200

    imageRegistry: ensure the "local-container-registry" pod is running
    The previous code just checked that the container existed.
    Also start it. A `podman start` for an already running container
    has no bad effects.
    
    Also, when creating a new container, use "--replace" to ensure
    that we can start the new container.

--- imageRegistry.py
+++ imageRegistry.py
@@ -46,6 +46,7 @@ class ImageRegistry:
 
         if ret.success() and self.rsh.run(shlex.join(['test', '-d', dir_name])).success():
             if not delete_all:
+                self.rsh.run(shlex.join(["podman", "start", CONTAINER_NAME]))
                 return dir_name, self.listen_port
             self._delete_all()
 
@@ -92,6 +93,7 @@ class ImageRegistry:
                     "run",
                     "--name",
                     CONTAINER_NAME,
+                    "--replace",
                     "-p",
                     f"{self.listen_port}:5000",
                     "-v",
