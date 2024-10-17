263a461da101d0fd71fa27f2d7a60cd11168a330
Thomas Haller <thaller@redhat.com>
2024-10-17 11:47:49 +0200

    common: use @strict_dataclass from "ktoolbox.common"
    @strict_dataclass got copied from here to ktoolbox. It also
    improved there in the meantime.
    
    Use the improved version and don't have a re-implementation
    of it.

--- common.py
+++ common.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-import dataclasses
 import ipaddress
 from typing import Any, Callable, Optional, TypeVar, Iterator, Type
 from concurrent.futures import Future
@@ -25,6 +24,7 @@ import time
 import itertools
 import signal
 import types
+import ktoolbox.common as kcommon
 
 
 def with_timeout(timeout: int, func: Callable[[], None]) -> None:
@@ -44,67 +44,6 @@ def with_timeout(timeout: int, func: Callable[[], None]) -> None:
 
 
 T = TypeVar("T")
-
-
-def check_type(value: typing.Any, type_hint: typing.Any) -> bool:
-
-    # Some naive type checking. This is used for ensuring that data classes
-    # contain the expected types (via @strict_dataclass.
-    #
-    # That is most interesting, when we initialize the data class with
-    # data from an untrusted source (like elements from a JSON parser).
-
-    actual_type = typing.get_origin(type_hint)
-    if actual_type is None:
-        return isinstance(value, type_hint)
-
-    if actual_type is typing.Union:
-        args = typing.get_args(type_hint)
-        return any(check_type(value, a) for a in args)
-
-    if actual_type is list:
-        args = typing.get_args(type_hint)
-        (arg,) = args
-        return isinstance(value, list) and all(check_type(v, arg) for v in value)
-
-    if actual_type is dict:
-        args = typing.get_args(type_hint)
-        (arg_key, arg_val) = args
-        return isinstance(value, dict) and all(check_type(k, arg_key) and check_type(v, arg_val) for k, v in value.items())
-
-    if actual_type is tuple:
-        # tuple[int, ...] is not supported (yet).
-        args = typing.get_args(type_hint)
-        return isinstance(value, tuple) and len(value) == len(args) and all(check_type(value[i], args[i]) for i in range(len(value)))
-
-    return False
-
-
-TCallable = typing.TypeVar("TCallable", bound=typing.Callable[..., typing.Any])
-
-
-def strict_dataclass(cls: TCallable) -> TCallable:
-
-    init = getattr(cls, '__init__')
-
-    def wrapped_init(self, *args, **argv):  # type: ignore
-        init(self, *args, **argv)
-        for field in dataclasses.fields(self):
-            name = field.name
-            value = getattr(self, name)
-            type_hint = field.type
-            if not check_type(value, type_hint):
-                raise TypeError(f"Expected type '{type_hint}' for attribute '{name}' but received type '{type(value)}')")
-
-        # Normally, data classes support __post_init__(), which is called by __init__()
-        # already. Add a way for a @strict_dataclass to add additional validation *after*
-        # the original check.
-        _post_check = getattr(type(self), "_post_check", None)
-        if _post_check is not None:
-            _post_check(self)
-
-    setattr(cls, '__init__', wrapped_init)
-    return cls
 
 
 def str_to_list(input_str: str) -> list[int]:
@@ -206,7 +145,7 @@ class RangeList:
 RangeList.UNLIMITED = RangeList()
 
 
-@strict_dataclass
+@kcommon.strict_dataclass
 @dataclass
 class IPRouteAddressInfoEntry:
     family: str
@@ -217,7 +156,7 @@ class IPRouteAddressInfoEntry:
             raise ValueError("Invalid address family")
 
 
-@strict_dataclass
+@kcommon.strict_dataclass
 @dataclass
 class IPRouteAddressEntry:
     ifindex: int
@@ -326,7 +265,7 @@ def ip_addrs(rsh: host.Host, *, strict_parsing: bool = False, ifname: Optional[s
     return ip_addrs_parse(ret.out, strict_parsing=strict_parsing, ifname=ifname)
 
 
-@strict_dataclass
+@kcommon.strict_dataclass
 @dataclass
 class IPRouteLinkEntry:
     ifindex: int
@@ -367,7 +306,7 @@ def ip_links(rsh: host.Host, *, strict_parsing: bool = False, ifname: Optional[s
     return ip_links_parse(ret.out, strict_parsing=strict_parsing, ifname=ifname)
 
 
-@strict_dataclass
+@kcommon.strict_dataclass
 @dataclass
 class IPRouteRouteEntry:
     dst: str
